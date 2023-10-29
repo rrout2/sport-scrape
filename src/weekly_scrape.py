27230611feb88302a09bac3f683d2f70a0dcef88
Rishav Rout <rout.rishav@gmail.com>
2023-10-28 22:55:01 -0700

    data output

--- src/weekly_scrape.py
+++ src/weekly_scrape.py
@@ -5,17 +5,22 @@ from selenium.webdriver.remote.webelement import WebElement
 
 from scraper import Scraper
 import argparse
+import json
+import os
 
 parser = argparse.ArgumentParser(description='Description of your program')
-parser.add_argument('-y','--year', help='Which season', required=False)
-parser.add_argument('-l','--league-id', help='The ESPN ID of your league', required=True)
+parser.add_argument('-y','--year',       help='Which season',                       required=False)
+parser.add_argument('-n','--name',       help='name of the league',                 required=False)
+parser.add_argument('-l','--league-id',  help='The ESPN ID of your league',         required=True)
 parser.add_argument('-s','--start-week', help='The first week you want to scrape.', required=True)
-parser.add_argument('-e','--end-week', help='The last week you want to scrape.', required=True)
+parser.add_argument('-e','--end-week',   help='The last week you want to scrape.',  required=True)
+
 args = vars(parser.parse_args())
 league_id = args['league_id']
 start_week= int(args['start_week'])
 end_week = int(args['end_week'])
 year = '2023' if 'year' not in args else args['year']
+league_name = league_id if 'name' not in args else args['name']
 
 def weekly_scrape(url: str):
     scraper = Scraper()
@@ -53,10 +58,15 @@ def weekly_scrape(url: str):
     return scoreboard
 
 def assemble_url(season_id: str, week: str):
-    fmtStr = 'https://fantasy.espn.com/football/boxscore?leagueId={leagueId}&matchupPeriodId={week}&scoringPeriodId={week}&seasonId={season_id}'
-    return fmtStr.format(season_id, week , league_id)
+    fmtStr = 'https://fantasy.espn.com/football/boxscore?leagueId={league_id}&matchupPeriodId={week}&scoringPeriodId={week}&seasonId={season_id}'
+    return fmtStr.format(season_id = season_id, week = week, league_id = league_id)
 
 weeks = [assemble_url(year, week) for week in range(start_week, end_week + 1)]
 
-for week in weeks:
-    weekly_scrape(week)
+for i, week in enumerate(weeks):
+    scoreboard = weekly_scrape(week)
+    filename = 'data/{league_name}/week_{week_num}.json'.format(league_name = league_name, week_num = i)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    f = open(filename, 'w')
+    f.write(json.dumps(scoreboard))
+    f.close()
